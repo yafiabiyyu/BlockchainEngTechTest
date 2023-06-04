0a3d933f211735740aa62e99de94cfc77961bf67
Abiyyu <yafiabiyyu@protonmail.com>
2023-06-04 20:18:55 +0700

    remove

--- contracts/chainlink/VRFCoordinatorV2.sol
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-import "@chainlink/contracts/src/v0.8/VRFCoordinatorV2.sol";
--- contracts/interfaces/IVote.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
-
-interface IVote {
-    function initialize(string calldata votingName, address owner) external;
-}
