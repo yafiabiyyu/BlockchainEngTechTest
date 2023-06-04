226d46c21b97e492851a6471c637ba340a836cf4
Abiyyu <yafiabiyyu@protonmail.com>
2023-06-05 03:30:54 +0700

    update depends

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,40 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config({path:".env"})
+require("@nomicfoundation/hardhat-chai-matchers");
+require("dotenv").config({ path: ".env" });
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    compilers:[
-      {
-        version: "0.8.15"
-      },
-      {
-        version: "0.4.24"
-      }
-    ]
-  },
-  networks:{
-    goerli: {
-      url: process.env.RPC,
-      chainId: 5,
-      accounts:{
-        mnemonic: process.env.MNEMONIC,
-        path: "m/44'/60'/0'/0",
-      }
-    }
-  }
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.15",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 2000,
+                    },
+                },
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
+    },
+    networks: {
+        goerli: {
+            url: process.env.RPC,
+            chainId: 5,
+            accounts: {
+                mnemonic: process.env.MNEMONIC,
+                path: "m/44'/60'/0'/0",
+            },
+        },
+    },
+    gasReporter: {
+        token: "CELO",
+        currency: "USD",
+        gasPrice: 21,
+        enabled: true,
+    },
 };
